5f97ce46c172a29d27fb3bf301edc797625ccc36
baddou <baddou.mustapha@gmail.com>
2019-11-28 14:27:08 +0100

    first

---
+++ WebApplication9/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult GetView()
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "baddou";
+            emp.LastName = "mustapha";
+            emp.Salary = 10000;
+            ViewBag.emp = emp;
+            return View("MyView");
+        }
+    }
+}
